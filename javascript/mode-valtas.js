40f42ea1803fc5c8c63f49ece104698bd308786f
Kovács Péter <peter.kovacs@umszki.hu>
2026-03-15 02:28:34 +0100

    ?

--- javascript/mode-valtas.js
+++ javascript/mode-valtas.js
@@ -1,21 +1,21 @@
-let lightmode = localStorage.getItem('lightmode')
-const kapcsolo = document.getElementById('kapcsolo')
+let lightmode = localStorage.getItem('lightmode');
+const kapcsolo = document.getElementById('kapcsolo');
 
 const bekapcsLightmode = () => {
-    document.body.classList.add('lightmode')
-    localStorage.setItem('lightmode', 'bekapcsolt')
+    document.body.classList.add('lightmode');
+    localStorage.setItem('lightmode', 'bekapcsolt');
 }
 
 const kikapcsLightmode = () => {
-    document.body.classList.remove('lightmode')
-    localStorage.setItem('lightmode', null)
+    document.body.classList.remove('lightmode');
+    localStorage.removeItem('lightmode');
 }
 
-if (lightmode === 'bekapcsolt') bekapcsLightmode()
+if (lightmode === 'bekapcsolt') bekapcsLightmode();
 
-kapcsolo.addEventListener(
-    "click", () => {
-            lightmode = localStorage.getItem('lightmode')
-            lightmode !== "bekapcsolt" ? bekapcsLightmode() : kikapcsLightmode()
+kapcsolo.addEventListener("click", () => {
+    lightmode = localStorage.getItem('lightmode');
+    lightmode !== "bekapcsolt"
+        ? bekapcsLightmode()
+        : kikapcsLightmode();
 });
-
